1f6845c04e8a76ba926f24c73d8846b2c866b515
Robert Pankowecki <robert.pankowecki@gmail.com>
2011-11-03 22:11:05 +0100

    read without newlines

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,8 +1,8 @@
 class PapersController < ApplicationController
 
   before_filter :only => [:index, :destroy] do
-    user = File.open(File.join(Rails.root, *%w(config user.cnf)) ){|f| f.read }
-    pass = File.open(File.join(Rails.root, *%w(config pass.cnf)) ){|f| f.read }
+    user = File.open(File.join(Rails.root, *%w(config user.cnf)) ){|f| f.read }.strip
+    pass = File.open(File.join(Rails.root, *%w(config pass.cnf)) ){|f| f.read }.strip
 
     unless(authenticate_with_http_basic {|u, p| user == u && pass == p })
       request_http_basic_authentication and return
