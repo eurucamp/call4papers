cd9f0d4dfb3c89accb748506eb3067cce5bc1c1e
Paweł Pacana <pawel.pacana@gmail.com>
2011-11-03 11:52:33 +0100

    capistrano: typo, system rvm, sudo cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ require 'rvm/capistrano'
 
 set :application, "cfp"
 set :user, "#{application}"
-set :sevice, "#{application}"
+set :service, "#{application}"
 set :deploy_to, "/srv/#{application}"
 set :deploy_via, :remote_cache
 set :repository, "git@github.com:drugpl/call4papers.git"
@@ -12,7 +12,6 @@ set :ssh_options, {:forward_agent => true}
 set :keep_releases, 5
 set :bundle_without, %w(test development)
 set :rvm_ruby_string, '1.9.2'
-set :rvm_type, :user
 set :use_sudo, false
 
 default_run_options[:pty] = true
@@ -23,24 +22,19 @@ role :db,  "wrocloverb-cfp", :primary => true
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    # run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
-
-  end
-
-  task :start, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} sv start /etc/service/#{fetch(:service)}"
+    run "sudo sv start /etc/service/#{fetch(:service)}"
   end
 
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} sv stop /etc/service/#{fetch(:service)}"
+    run "sudo sv stop /etc/service/#{fetch(:service)}"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} sv restart /etc/service/#{fetch(:service)}"
+    run "sudo sv restart /etc/service/#{fetch(:service)}"
   end
 
   task :status, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} sv status /etc/service/#{fetch(:service)}"
+    run "sudo sv status /etc/service/#{fetch(:service)}"
   end
 end
 
