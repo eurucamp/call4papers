9f9833aa1c7106bf06853fe2add09d3a30dc22cc
Robert Pankowecki <robert.pankowecki@gmail.com>
2011-11-03 22:19:05 +0100

    action_mailer.default_url_options for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Cfp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "c4p.wrocloverb.com" }
 
   # Enable threaded mode
   # config.threadsafe!
