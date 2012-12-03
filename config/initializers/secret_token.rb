dae66891f2e555375f90f540acf0dc759385e519
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-04 00:24:15 +0100

    Remove secret token from repo.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cfp::Application.config.secret_token = '1a1c2885358c22de07c03b81221b404ada6707f4a12bbfbf0391d3dca13783529ff141b247e8e4c2e04cd2fe7096a255941c7de36a3412ddd94b8d0dbe7667b3'
