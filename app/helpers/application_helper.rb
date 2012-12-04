23b1bf2c3df28b3c6aa2348b829196b30f721913
Michał Łomnicki <michal.lomnicki@gmail.com>
2012-12-04 02:26:49 +0100

    Escape text before processing it with markdown.
    Otherwise it's vulnerable to XSS.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def markdown(text)
     renderer = Redcarpet::Render::HTML.new(:hard_wrap => true)
     markdown = Redcarpet::Markdown.new(renderer, :autolink => true, :space_after_headers => true, :fenced_code_blocks => true, :no_intra_emphasis => true)
-    markdown.render(text).html_safe
+    markdown.render(h(text)).html_safe
   end
 end
