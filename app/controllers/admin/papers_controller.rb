2df702b9d0b6def1c2138e0c62a141dd10b810c2
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-07 00:12:15 +0100

    Root path is better.

--- app/controllers/admin/papers_controller.rb
+++ app/controllers/admin/papers_controller.rb
@@ -1,13 +1,13 @@
 class Admin::PapersController < ApplicationController
   before_filter :allow_staff_only
-  
+
   def index
     @papers = Paper.all
   end
-  
+
   private
-  
+
   def allow_staff_only
-    redirect_to authentications_path unless current_user.staff?
+    redirect_to root_path unless current_user.staff?
   end
 end
