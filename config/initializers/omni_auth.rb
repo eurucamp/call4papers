a5f7ed58841ffb84d95392fe27d0332f19777f8d
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-06 18:13:28 +0100

    We don't need rw access to profile, sorry for that.
    No scope is enough, according to:
    http://developer.github.com/v3/oauth/#scopes

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
