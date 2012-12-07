35cd13ab79773ab8bb3873e56dd88735a09334bb
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-07 01:11:58 +0100

    Better with helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(renderer, :autolink => true, :space_after_headers => true, :fenced_code_blocks => true, :no_intra_emphasis => true)
     markdown.render(h(text)).html_safe
   end
+
+  def twiiter_link(handle)
+    link_to(handle, "http://twiiter.com/#{handle}").html_safe if handle
+  end
+
+  def github_link(handle)
+    link_to(handle, "http://github.com/#{handle}").html_safe if handle
+  end
 end
