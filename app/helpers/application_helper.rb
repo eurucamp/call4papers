ca3275e6a8879b59af755d8ddfd7600f2f4b85af
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-05 23:26:34 +0100

    Add avatars to profile listing and profile preview.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,18 @@ module ApplicationHelper
     handle = h(handle.to_s.sub(/\A@/, ''))
     link_to(truncate(handle, length: 20), "http://github.com/#{handle}") unless handle.blank?
   end
+
+  def gravatar_avatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
+  def twitter_avatar_url(handle)
+    "https://twitter.com/api/users/profile_image/#{handle}?size=bigger"
+  end
+
+  def avatar_url(user, size = 64)
+    return twitter_avatar_url(user.twitter_handle) if user.twitter_handle
+    gravatar_avatar_url(user, size)
+  end
 end
