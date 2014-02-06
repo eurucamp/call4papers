e87cb19a5761b9ce2fbbfe8a3a38a0410f8c2df1
Alex Coles <alex@alexbcoles.com>
2014-02-06 20:02:19 +0100

    Update schema.rb to new 1.9+ Hash syntax
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,43 +11,43 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402130008) do
+ActiveRecord::Schema.define(version: 20130402130008) do
 
-  create_table "authentications", :force => true do |t|
+  create_table "authentications", force: true do |t|
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "papers", :id => false, :force => true do |t|
-    t.string   "id",                                     :null => false
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "title",                                  :null => false
+  create_table "papers", id: false, force: true do |t|
+    t.string   "id",                                     null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "title",                                  null: false
     t.text     "public_description"
-    t.text     "private_description",                    :null => false
-    t.integer  "user_id",                                :null => false
-    t.boolean  "selected",            :default => false, :null => false
+    t.text     "private_description",                    null: false
+    t.integer  "user_id",                                null: false
+    t.boolean  "selected",            default: false, null: false
     t.string   "time_slot"
     t.string   "track"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "name",                                   :null => false
-    t.string   "email",                                  :null => false
+  create_table "users", force: true do |t|
+    t.string   "name",                                   null: false
+    t.string   "email",                                  null: false
     t.text     "bio"
     t.string   "website_url"
     t.string   "twitter_handle"
     t.string   "github_handle"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(:version => 20130402130008) do
     t.boolean  "staff"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
