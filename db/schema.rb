556b3eb48f7e81eba8a7c8ad00ab3b6777277355
Alex Coles <alex@alexbcoles.com>
2014-02-08 11:36:23 +0100

    Create migration to add missing foreign keys
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206191741) do
+ActiveRecord::Schema.define(version: 20140208095705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,5 +74,10 @@ ActiveRecord::Schema.define(version: 20140206191741) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "authentications", "users", name: "authentications_user_id_fk", dependent: :delete
+
+  add_foreign_key "papers", "calls", name: "papers_call_id_fk", dependent: :restrict
+  add_foreign_key "papers", "users", name: "papers_user_id_fk", dependent: :delete
 
 end
