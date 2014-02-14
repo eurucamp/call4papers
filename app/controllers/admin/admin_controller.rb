b8a7ec6881468aadff17eebdaa0739385acb0a07
Alex Coles <alex@alexbcoles.com>
2014-02-14 20:35:56 +0100

    Add initial admin CSV export for Papers, Users
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -6,4 +6,17 @@ class Admin::AdminController < ApplicationController
   def allow_staff_only
     redirect_to root_path unless current_user.staff?
   end
+
+  protected
+
+  def build_csv(object, columns)
+    require 'csv'
+
+    csv_response = CSV.generate do |csv|
+      csv << columns
+      object.each do |paper|
+        csv << columns.map { |col| paper.send(col) }
+      end
+    end
+  end
 end
--- test/functional/admin/users_controller_test.rb
+++ test/functional/admin/users_controller_test.rb
@@ -11,4 +11,16 @@ class Admin::UsersControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test 'should get export (CSV)' do
+    sign_in users(:admin_user)
+
+    get :export, format: 'csv'
+    assert_response :success
+
+    csv = CSV.parse(response.body, headers: true)
+    assert_equal 'id',    csv.headers[0]
+    assert_equal 'name',  csv.headers[1]
+    assert_equal 3,       csv.size
+  end
 end
