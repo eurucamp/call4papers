7aa9ce37c10b8aacdec7e6ddfd3323bf3d2c995a
Florian Gilcher <florian.gilcher@asquera.de>
2014-04-25 05:55:24 +0000

    Add a host to the production env
    makes password reset work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Cfp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'cfp.eurucamp.org' }
 end
 
 ActionMailer::Base.smtp_settings = {
