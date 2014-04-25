94ab4244e1ebc0b9aa9682f2123b5cef9185d4c4
Florian Gilcher <florian.gilcher@asquera.de>
2014-04-25 06:01:38 +0000

    Only show papers for open calls

--- app/controllers/mentor/papers_controller.rb
+++ app/controllers/mentor/papers_controller.rb
@@ -1,5 +1,5 @@
 class Mentor::PapersController < Mentor::MentorController
   def index
-    @papers = Paper.not_from(current_user).mentors_can_read.order('created_at DESC')
+    @papers = Paper.for_open_call.not_from(current_user).mentors_can_read.order('created_at DESC')
   end
 end
