c49702cd210a27af6232640b196c857566e4e376
José Tomás Albornoz <jojo@eljojo.net>
2014-04-27 16:48:42 +0200

    Testing feedback mail sending

---
+++ test/functional/mentor/feedbacks_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class Mentor::FeedbacksControllerTest < ActionController::TestCase
+  setup do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
+  test 'valid feedback mails are delivered' do
+    paper = papers(:one)
+    feedback = Feedback.new(feedback: 'Nice job')
+
+    sign_in users(:mentor_user)
+
+    post :contact, id: paper.id, feedback: { feedback: feedback.feedback }
+
+    assert_not ActionMailer::Base.deliveries.empty?
+    mail = ActionMailer::Base.deliveries.last
+    assert mail.body.include?(feedback.feedback)
+  end
+
+  test 'empty feedbacks mails are not delivered' do
+    paper = papers(:one)
+    feedback = Feedback.new(feedback: '')
+
+    sign_in users(:mentor_user)
+
+    post :contact, id: paper.id, feedback: { feedback: feedback.feedback }
+
+    assert ActionMailer::Base.deliveries.empty?
+  end
+end
