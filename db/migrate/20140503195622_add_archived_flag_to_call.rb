05a2066d33bc2219107a93bd30e7f8b307acfc20
Florian Gilcher <florian.gilcher@asquera.de>
2014-05-03 20:15:51 +0000

    Make sure archived is off by default

--- db/migrate/20140503195622_add_archived_flag_to_call.rb
+++ db/migrate/20140503195622_add_archived_flag_to_call.rb
@@ -1,6 +1,6 @@
 class AddArchivedFlagToCall < ActiveRecord::Migration
   def change
-    add_column :calls, :archived, :boolean
+    add_column :calls, :archived, :boolean, default: false
  
     Call.all { |c| c.archived = false; c.save }
   end
