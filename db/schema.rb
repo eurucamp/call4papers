b8f57df94a6fe667cbae83f204aba719fd171422
Martin Luder <maser@winterfell.ch>
2015-03-21 17:58:20 +0100

    Remove selected column from talks
    Proposals are selected (for a conference), not talks.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150321155539) do
+ActiveRecord::Schema.define(version: 20150321165746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,6 @@ ActiveRecord::Schema.define(version: 20150321155539) do
     t.string   "title",                              null: false
     t.string   "public_description"
     t.string   "private_description",                null: false
-    t.boolean  "selected"
     t.string   "time_slot"
     t.string   "track"
     t.string   "mentor_name"
