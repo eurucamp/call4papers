a4212039ca5103036b05913939120b73d0c20594
Jan Schulte <hello@unexpected-co.de>
2015-03-21 18:01:09 +0100

    This fixes broken tests for proposals/talks
    This is not completely finished. It needs to be adjusted when users can submit talks to multiple
    conferences. Right now it assumes that a talk has only one proposal

--- test/functional/proposals_controller_test.rb
+++ test/functional/proposals_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ProposalsControllerTest < ActionController::TestCase
   setup do
     @request.env['devise.mapping'] = Devise.mappings[:user]
-    @proposal = proposals(:one)
+    @talk = talks(:one)
     @call  = calls(:one)
     sign_in users(:rockstar)
   end
@@ -19,11 +19,14 @@ class ProposalsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create proposal" do
+  test "should create talk" do
     assert_difference('Proposal.count') do
-      post :create, proposal: @proposal.attributes, call_id: @call.id
+      post :create, talk: @talk.attributes, call_id: @call.id
     end
+  end
 
+  test "should redirect to proposal_path after creating talk" do
+    post :create, talk: @talk.attributes, call_id: @call.id
     assert_redirected_to proposal_path(assigns(:proposal))
   end
 
@@ -32,37 +35,35 @@ class ProposalsControllerTest < ActionController::TestCase
     assert_response 400
   end
 
-  test 'should not create a proposal given invalid attributes' do
-    skip('will get fixed by getting proposal creation to work')
-    post :create, proposal: { title: nil }, call_id: @call.id
+  test 'should not create a talk given invalid attributes' do
+    post :create, talk: { title: nil }, call_id: @call.id
     assert_response :success
     assert_template :new
   end
 
   test "should show proposal" do
-    get :show, id: @proposal.to_param
+    get :show, id: @talk.to_param
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @proposal.to_param
+    get :edit, id: @talk.to_param
     assert_response :success
   end
 
   test "should update proposal" do
-    put :update, id: @proposal.to_param, proposal: @proposal.attributes
+    put :update, id: @talk.to_param, talk: @talk.attributes
     assert_redirected_to proposal_path(assigns(:proposal))
   end
 
   test 'should not update a proposal given invalid attributes' do
-    skip('will get fixed by getting proposal updates to work')
-    put :update, id: @proposal.to_param, proposal: { title: nil }
+    put :update, id: @talk.to_param, talk: { title: nil }
     assert_template :edit
   end
 
   test "should destroy proposal" do
     assert_difference('Proposal.count', -1) do
-      delete :destroy, id: @proposal.to_param
+      delete :destroy, id: @talk.to_param
     end
 
     assert_redirected_to proposals_path
